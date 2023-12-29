6d36637ef074f464b338e04dcac9646f13a1d7df
Gurdev Sihra <sihragurdev@gmail.com>
2023-12-29 11:16:15 -0800

    Basic Mongodb Access (#1)

---
+++ index.js
@@ -0,0 +1,41 @@
+var config = require('./config');
+//log_in(config.username, config.password);
+var Express = require("express");
+const mongodb = require("mongodb");
+var MongoClient = mongodb.MongoClient;
+// Allows any traffic from any IP address
+var cors=require("cors");
+// Handles form data for uploading files
+const multer=require("multer"); 
+
+var app=Express();
+app.use(cors());
+
+
+
+
+
+var CONNECTION_STRING="mongodb+srv://"+config.username+":"+config.password+"@cluster0.0s6fyy3.mongodb.net/?retryWrites=true&w=majority";
+console.log(CONNECTION_STRING);
+
+
+let db = null;
+
+var DATABASE_NAME="MonsterClassifier";
+var database;
+
+app.listen(5038,()=>{
+    MongoClient.connect(CONNECTION_STRING,(error,client)=>{
+        if(error){
+            throw error;
+        }
+        db=client.db(DATABASE_NAME);
+        console.log("Mongo DB Connection Successful");
+    });
+})
+
+app.get('/humans',(request,response)=>{
+    db.collection("Human").find({}).toArray((error,result)=> {
+        response.send(result);
+    });
+})
