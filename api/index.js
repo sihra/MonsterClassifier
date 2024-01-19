f24ce4db4e2a6531d6db921720051bf00a59b8a4
sihra <sihragurdev@gmail.com>
2024-01-18 20:22:19 -0800

    Working User Image Upload

--- api/index.js
+++ api/index.js
@@ -1,51 +1,112 @@
 var config = require('./config');
-//log_in(config.username, config.password);
 var Express = require("express");
 const mongodb = require("mongodb");
+const path = require("path");
 var MongoClient = mongodb.MongoClient;
 // Allows any traffic from any IP address
-var cors=require("cors");
+var cors = require("cors");
 // Handles form data for uploading files
-const multer=require("multer"); 
+const multer = require("multer");
 
-var app=Express();
+var app = Express();
 app.use(cors());
 
 
-var CONNECTION_STRING="mongodb+srv://"+config.username+":"+config.password+"@cluster0.0s6fyy3.mongodb.net/?retryWrites=true&w=majority";
+var CONNECTION_STRING = "mongodb+srv://" + config.username + ":" + config.password + "@cluster0.0s6fyy3.mongodb.net/?retryWrites=true&w=majority";
 console.log(CONNECTION_STRING);
 
-
 let db = null;
-
-var DATABASE_NAME="MonsterClassifier";
+var DATABASE_NAME = "MonsterClassifier";
 var database;
 
-app.listen(5038,()=>{
-    MongoClient.connect(CONNECTION_STRING,(error,client)=>{
-        if(error){
+//Setting storage engine for multer
+// ./potentialMonsters will be available for users to upload images to
+// Using Date.now() for file upload
+// Req = request, file = file, cb = callback which determines the uploaded file's name
+const storageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './potentialMonsters');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}--${file.originalname}`);
+    },
+});
+
+// Upload limits on types of file a user can upload
+// We also defined a function that filters on 
+const upload = multer({
+    storage: storageEngine,
+    limits: { fileSize: 15000000 },
+    fileFilter: function (req, file, callback) {
+        console.log("in check!");
+        var ext = path.extname(file.originalname);
+        if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+            return callback(new Error('Only images are allowed'))
+        }
+        callback(null, true)
+    },
+});
+
+
+const checkFileType = function(file,cb) {
+    //Regex ecpression containing allowed file extensions
+  const fileTypes = /jpeg|jpg|png/;
+
+  console.log("in check!");
+
+
+  // Check extension names
+  const extensionName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimeType = fileTypes.test(file.mimeType)
+
+  console.log("in check!");
+
+    if (mimeType && extensionNames) {
+        cb(null,true);
+    } else {
+        cb("Error! You can only upload images! (,,>﹏<,,)")
+    }
+
+}
+
+
+
+app.listen(5038, () => {
+    MongoClient.connect(CONNECTION_STRING, (error, client) => {
+        if (error) {
             throw error;
         }
-        db=client.db(DATABASE_NAME);
+        db = client.db(DATABASE_NAME);
         console.log("Mongo DB Connection Successful");
     });
 })
 
-app.get('/humans',(request,response)=>{
-    db.collection("Human").find({}).toArray((error,result)=> {
+app.get('/humans', (request, response) => {
+    db.collection("Human").find({}).toArray((error, result) => {
         response.send(result);
     });
 })
 
-app.post('/postMonster',multer().none(),(request,response)=>{
+app.post('/postMonster', multer().none(), (request, response) => {
     console.log(request.body.classification);
-    db.collection("Human").count({},function(error,numOfDocs){
+    db.collection("Human").count({}, function (error, numOfDocs) {
         db.collection("Human").insertOne({
-            id:(numOfDocs+1).toString(),
-            classification:request.body.classification,
+            id: (numOfDocs + 1).toString(),
+            classification: request.body.classification,
             image_url: request.body.image
         });
         response.json("Added Succesfully");
     })
 })
 
+app.post("/postMe", upload.single("file"), (req, res) => {
+
+    console.log("Ouch! You hit me!");
+    console.log(res);
+    if (req.file) {
+      res.send("Single file uploaded successfully");
+    } else {
+      res.status(400).send("Please upload a valid image");
+    }
+    res.json("Uploaded image successfully");
+  });
--- ui/monsterclassifier/src/App.js
+++ ui/monsterclassifier/src/App.js
@@ -1,77 +1,162 @@
 import './App.css';
 import { Component } from 'react';
-class App extends Component{
+class App extends Component {
 
-  constructor(props){
+
+  constructor(props) {
     // Initialize parent constructor as well
     super(props);
-    this.state={
-      urls:[]
+    this.state = {
+      urls: [],
+      monsterImage: { preview: "", data: ""},
+      status: "",
     }
   }
 
-  API_URL="http://localhost:5038"
+  API_URL = "http://localhost:5038"
 
-  componentDidMount(){
+  componentDidMount() {
     this.refreshPage();
   }
 
-  async humanClick(){
+  async handleSubmit(e) {
+    //const [count, setCount] = useState(0);
+    e.preventDefault()
+   
+    const data = new FormData();
+
+    const peep = this.status
+
+    //alert({peep})
+
+    //alert("hi");
+    
+    data.append("file", this.state.monsterImage.data)
+    
+
+    this.setState({ status: "... Detecting Monster ..." })
+
+    fetch(this.API_URL + "/postMe", {
+      method: "POST",
+      body: data,
+    }).then(res => res.json()).then((result) => {
+      //this.status = (result.statusText)
+      this.setState({ status: result.statusText })
+      //alert(result);
+      //this.refreshPage();
+    })
+    console.log("Submitted")
+  }
+
+  async handleFileChange(e) {
+    console.log("File Change")
+    // const img = {
+    //   preview: URL.createObjectURL(e.target.files[0]),
+    //   data: e.target.files[0],
+    // }
+    // this.setState({ monsterImage: img })
+
+
+
+    // this.setState((status) =>{
+    //   return "testing fured"
+    // });
+
+    const peep = this.status
+
+    //alert({peep} + " hello")
+
+    this.setState({
+      monsterImage: {
+        //...this.state.monsterImage, 
+        preview: URL.createObjectURL(e.target.files[0]), 
+        data: e.target.files[0]
+        }
+    },
+    function() {
+      console.log("setState completed", this.state)
+    });
+
+    
+    //this.monsterImage = img
+    console.log("File Change")
+
+  }
+
+  async humanClick() {
     var image = document.getElementById("imageUpload").value;
     console.log("Uploading Image " + image)
     const data = new FormData();
     data.append("image", image);
     data.append("classification", "human");
 
-    fetch(this.API_URL+"/postMonster",{
-      method:"POST",
+    fetch(this.API_URL + "/postMonster", {
+      method: "POST",
       body: data,
-    }).then(res=>res.json()).then((result)=>{
+    }).then(res => res.json()).then((result) => {
       alert(result);
       this.refreshPage();
     })
   }
 
-  async monsterClick(){
+  async monsterClick() {
     var image = document.getElementById("imageUpload").value;
     console.log("Uploading Image " + image)
     const data = new FormData();
     data.append("image", image);
     data.append("classification", "monster");
 
-    fetch(this.API_URL+"/postMonster",{
-      method:"POST",
+    fetch(this.API_URL + "/postMonster", {
+      method: "POST",
       body: data,
-    }).then(res=>res.json()).then((result)=>{
+    }).then(res => res.json()).then((result) => {
       alert(result);
       this.refreshPage();
     })
   }
 
-async refreshPage(){
-  fetch(this.API_URL+"/Humans").then(response=>response.json())
-  .then(data=>{
-    this.setState({urls:data})
-  })
-}
+  async refreshPage() {
+    fetch(this.API_URL + "/Humans").then(response => response.json())
+      .then(data => {
+        this.setState({ urls: data })
+      })
+  }
   // imageUpload = File Upload for photo
-  
+
   render() {
-    const{urls}=this.state
+    const { urls, monsterImage, status } = this.state
     return (
       <div className="App">
-        <h2>Monster Classifier</h2>
-    
-        <input id="imageUpload"/>&nbsp;
-        <button onClick={()=>this.humanClick()}>Human</button>
-        <button onClick={()=>this.monsterClick()}>Monster</button>
+        <h2>Reformatted Monster Classifier</h2>
+        <h1>Upload to server</h1>
 
-        {urls.map(urlm=>
+        {monsterImage.preview && <img src={monsterImage.preview} width='100' height='100' />}
+
+        <hr></hr>
+        <form onSubmit={(event) => {this.handleSubmit(event)} }>
+          <input type='file' name='file' onChange={(event) => { this.handleFileChange(event); }
+          }></input>
+
+          <button type='submit'>Submit</button>
+
+        </form>
+        {status && <h4>{status}</h4>}
+        <h4>{status}</h4>
+
+        <h2>Gurdev</h2>
+
+        <h2>Monster Classifier</h2>
+
+        <input id="imageUpload" />&nbsp;
+        <button onClick={() => this.humanClick()}>Human</button>
+        <button onClick={() => this.monsterClick()}>Monster</button>
+
+        {urls.map(urlm =>
           <p>
             <b>URL: {urlm.image_url}</b>&nbsp;
             <b>Classification: {urlm.classification}</b>
           </p>)
-          }
+        }
       </div>
     );
   }
