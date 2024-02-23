7ca5d36b150751ea97c2f699b8c0389efcd40a12
sihra <sihragurdev@gmail.com>
2024-02-22 20:33:02 -0800

    Final formatting of website

--- ui/monsterclassifier/src/App.js
+++ ui/monsterclassifier/src/App.js
@@ -69,35 +69,31 @@ class App extends Component {
 
             <div class="flex_item">
               <p>
-               The server side was built with Node.js, Express, and React.js. The machine learning model was built using <a href="https://course.fast.ai/Lessons/lesson1.html">Fast.ai's Practical Deep Learning For Coders</a> Lesson 1.
-                I've followed along with the classes by creating <a href="https://www.kaggle.com/gsihra">Kaggle Notebooks</a> to work with the fast.ai library.
+                The server side was built with Node.js, Express, and React.js. The machine learning model was built using <a href="https://course.fast.ai/Lessons/lesson1.html">Fast.ai's Practical Deep Learning For Coders</a> Lesson 1.
+                I've followed along with the classes by creating <a href="https://www.kaggle.com/gsihra">Kaggle Notebooks</a> to work with the Fast.ai library.
               </p>
             </div>
 
             <div class="flex_item">
               <p>
-                Monster Classifier uses fast.ai's 
- 
-                I built this model by running a Kaggle notebook that fetches 
+                Monster Classifier uses Fast.ai's resnet18 neural network model which is integrated with a large collection of data vison models in the world (at the time). This makes it a great pick because it's a general model and can handle a variety of data.
 
-                I created a <a href="https://docs.fast.ai/data.block.html#datablock">DataBlock</a> which is used to train the model. It helps to prepare the data for the model I built.
+
+                I created a <a href="https://docs.fast.ai/data.block.html#datablock">DataBlock</a> which inputs data into this model and outlines the type of data we're expecting back (categories).
+
+                A <a href="https://docs.fast.ai/vision.learner.html">Vision Learner</a> was used to combine the model and data that I used to train it with. Then by using the <a href="which deploys my model and returns the probability of the image uploaded being a monster!">predict method</a>, MonsterClassifier is able to return the probability of the image uploaded being a monster!
               </p>
             </div>
 
-            
             <div class="flex_item">
-              <p>Overall, this lesson helped me understand how to use fast.ai's tool to create a model, but a lot of my knowledge behind how it was created and fine tuned is fuzzy.
-                As I continue with the course I believe I'll learn
+              <p>Overall, this lesson helped me understand how to use Fast.ai's tool to use and train a model, but a lot of my knowledge behind how it was created and fine tuned is fuzzy.
+                As I continue with the course I believe I'll understand the fundamental better.
                 The documentation is bare bones but I found a lot of help through the forums.
               </p>
             </div>
 
             <div class="flex_item">
-              <p>Due to limited time, the model has only been trained with 90 images retrieved from DuckDuckGo within 5 minutes. The model itself relies on </p>
-            </div>
-
-            <div class="flex_item">
-              <h4>Please upload an image that you'd like to see classified as a monster or not.</h4>
+              <h4>Please upload an image that you'd like to see classified as a monster or not. Submit when you're ready...</h4>
             </div>
 
             <div class="center_me">
